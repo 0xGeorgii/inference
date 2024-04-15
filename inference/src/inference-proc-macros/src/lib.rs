71dd0eb461791283ddf499edcf0853a293d2ff1f
Georgii Plotnikov <accembler@gmail.com>
2024-04-15 20:19:44 +0500

    suppress warnings for `inference_fun` macro

--- inference/src/inference-proc-macros/src/lib.rs
+++ inference/src/inference-proc-macros/src/lib.rs
@@ -2,6 +2,8 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
 
 #[proc_macro]
 pub fn inference(input: TokenStream) -> TokenStream {
@@ -15,5 +17,10 @@ pub fn inference_spec(attr: TokenStream, item: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn inference_fun(attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+    let mut input_function = parse_macro_input!(item as ItemFn);
+    let dead_code_attr = syn::parse_quote!(#[allow(dead_code)]);
+    input_function.attrs.push(dead_code_attr);
+    TokenStream::from(quote! {
+        #input_function
+    })
 }
